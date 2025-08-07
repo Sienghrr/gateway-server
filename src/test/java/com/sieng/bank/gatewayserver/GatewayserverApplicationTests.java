dba1cc385c40b1485929117d3aacf216a0ee0691
Sienghrr <siengsweetheart151@gmail.com>
2025-08-07 15:26:43 +0700

    api-gateway

---
+++ src/test/java/com/sieng/bank/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sieng.bank.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
